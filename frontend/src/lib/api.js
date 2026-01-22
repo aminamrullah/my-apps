c486cc64131f08f96bfe589544db65abf2405dce
ustadkoding <aminsuci123@gmail.com>
2026-01-22 09:58:00 +0700

    8th commit my-apps for stmik abang

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,3 +1,3 @@
 // export const API_BASE =
 //   import.meta.env.VITE_API_URL || "http://localhost:3000/api";
-export const API_BASE = "https://my-apps-c9hu.vercel.app/";
+export const API_BASE = "https://my-apps-c9hu.vercel.app";
